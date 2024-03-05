c6e090e89d33d17b9178ed67ca31b066614cbb86
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-03-05 15:46:11 +0530

    appbar fix

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,7 +1,12 @@
 import { Avatar } from "./BlogCard"
 import { Link } from "react-router-dom"
 
-export const Appbar = () => {
+interface AppbarProps{
+    authorName:string;
+}
+
+
+export const Appbar = ({authorName}:AppbarProps) => {
     return <div className="border-b flex justify-between px-10 py-4">
         <Link to={'/blogs'} className="flex flex-col justify-center cursor-pointer">
                 Medium
@@ -11,7 +16,7 @@ export const Appbar = () => {
                 <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">New</button>
             </Link>
 
-            <Avatar size={"big"} name="harkirat" />
+            <Avatar size={"big"} name={authorName || "jaimin"} />
         </div>
     </div>
 }
--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -13,7 +13,7 @@ export const Blog = () => {
 
     if (loading || !blog) {
         return <div>
-            <Appbar />
+            <Appbar authorName="Jaimin"/>
         
             <div className="h-screen flex flex-col justify-center">
                 
